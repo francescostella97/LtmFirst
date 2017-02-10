709645fc4195921ce4ef5d60aae956a1095fa63a
Francesco Stella <Francesco Stella>
2017-02-10 13:03:41 +0100

    Login

--- app/src/main/java/com/example/utente/ltmfirst/MainActivity.java
+++ app/src/main/java/com/example/utente/ltmfirst/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.utente.ltmfirst;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Utente on 08/02/2017.
@@ -10,9 +14,19 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    Button login_btn;
+    EditText username_et;
+    EditText psw_et;
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, )
+            }
+        });
     }
 }
