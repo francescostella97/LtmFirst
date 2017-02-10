1633dbb40de2e1eca2ef09d823f84702aa35a187
Francesco Stella <Francesco Stella>
2017-02-10 10:40:31 +0100

    Whole Project

---
+++ app/src/main/java/com/example/utente/ltmfirst/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.utente.ltmfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Utente on 08/02/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
