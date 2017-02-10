00b43fa231a227da5bc4c05a73d218d42c8b5b1a
Francesco Stella <Francesco Stella>
2017-02-10 17:11:09 +0100

    Contacts

--- app/src/main/java/com/example/utente/ltmfirst/LoginActivity.java
+++ app/src/main/java/com/example/utente/ltmfirst/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.utente.ltmfirst;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Vibrator;
 
 /**
  * Created by Utente on 10/02/2017.
@@ -15,31 +18,36 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     TextView login_btn;
-    EditText username_et;
+    EditText email_et;
     EditText psw_et;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login_btn.setEnabled(false);
+        email_et = (EditText) findViewById(R.id.login_email_txt);
+        psw_et = (EditText) findViewById(R.id.login_pwd_txt);
         login_btn = (TextView) findViewById(R.id.login_log_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            if(doLogin(username_et.getText().toString(), psw_et.getText().toString())) {
+            if(doLogin(email_et.getText().toString(), psw_et.getText().toString())) {
 
+                Vibrator x = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                x.vibrate(500);
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
             }
         });
     }
 
-    private boolean doLogin (String username, String password){
-        if (username.length()==0 || password.length()==0) {
-            login_btn.setEnabled(true);
+    private boolean doLogin (String email, String password) {
+        if (email.length() == 0 || password.length() == 0) {
+            Toast.makeText(LoginActivity.this,getText(R.string.login_res_ko),Toast.LENGTH_LONG).show();
             return false;
-        }
-        else return true;
+        } else return true;
+    }
 }
--- app/src/main/java/com/example/utente/ltmfirst/MainActivity.java
+++ app/src/main/java/com/example/utente/ltmfirst/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.utente.ltmfirst;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,17 +11,48 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.net.URI;
+
 /**
  * Created by Utente on 08/02/2017.
  */
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button go;
+    Button call;
+    TextView phoneNumber;
+    TextView address;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        phoneNumber = (TextView) findViewById(R.id.textTel);
+        address = (TextView) findViewById(R.id.textAddress);
+
+        go = (Button)findViewById(R.id.activity_go_btn);
+        call = (Button) findViewById(R.id.activity_call_btn);
+        go.setOnClickListener(this);
+        call.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.activity_call_btn){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("tel:" +phoneNumber.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+        }
+        else if (v.getId()==R.id.activity_go_btn){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("geo:0,0?=" +address.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+        }
     }
 }
